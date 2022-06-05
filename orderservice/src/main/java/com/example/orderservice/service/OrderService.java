9e51122dd02ea44fc0765606ebf25fd063177d29
meleksimal.unsal <meleksimal.unsal@trendyol.com>
2022-06-05 21:08:06 +0300

    order-service implementation

---
+++ orderservice/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.model.Order;
+
+public interface OrderService {
+    public Order saveOrder(Order order);
+}
---
+++ orderservice/src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.model.Order;
+import com.example.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
